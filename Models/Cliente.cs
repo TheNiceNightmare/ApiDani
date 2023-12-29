1671ce15c9dd865cc0325462d508d35e0e730324
GizloDani <daniel.olivo@gizlocorp.com>
2023-12-29 12:03:12 -0600

    si

--- Models/Cliente.cs
+++ Models/Cliente.cs
@@ -3,9 +3,9 @@ namespace ApiDani.Models
 {
     public class Cliente
     {
-        public int id { get; set; }
+        public string id { get; set; }
         public string nombre { get; set; }
-        public int edad { get; set; }
+        public string edad { get; set; }
         public string correo { get; set;}
     }
 }
