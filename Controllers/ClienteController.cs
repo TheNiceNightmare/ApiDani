0e735d5e3640efcad4cd9fb6b091f389e9f752c5
GizloDani <daniel.olivo@gizlocorp.com>
2023-12-29 11:56:21 -0600

    Agregar archivos de proyecto.

---
+++ Controllers/ClienteController.cs
@@ -0,0 +1,43 @@
+﻿using ApiDani.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace ApiDani.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ClienteController : ControllerBase // Reemplaza "cliente" por "controller"
+    {
+        [HttpGet]
+        [Route("listar")]
+        public dynamic listarCliente()
+        {
+            List<Cliente> clientes = new List<Cliente>
+            {
+                new Cliente
+                {
+                    id = "1",
+                    nombre = "Dani",
+                    edad = "20",
+                    correo = "Dani@gmail.com"
+                },
+                new Cliente
+                {
+                    id = "2",
+                    nombre = "Marce",
+                    edad = "21",
+                    correo = "Marce@gmail.com"
+                }
+            };
+
+            return clientes;
+        }
+
+        //[HttpPost]
+        //[Route("guardar")]
+        //public dynamic guardarCliente()
+        //{
+
+        //}
+    }
+}
---
+++ Models/Cliente.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ApiDani.Models
+{
+    public class Cliente
+    {
+        public int id { get; set; }
+        public string nombre { get; set; }
+        public int edad { get; set; }
+        public string correo { get; set;}
+    }
+}
